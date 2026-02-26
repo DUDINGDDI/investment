09d145205dcd6da4c40b1e8aa3d46381b6f2090b
MinChangJeong <wjdalsckd777@naver.com>
2026-02-26 17:41:23 +0900

    한국시간으로 타임존 설정

--- src/main/java/com/pm/investment/InvestmentApplication.java
+++ src/main/java/com/pm/investment/InvestmentApplication.java
@@ -1,12 +1,21 @@
 package com.pm.investment;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableScheduling
 public class InvestmentApplication {
+
+    @PostConstruct
+    void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(InvestmentApplication.class, args);
